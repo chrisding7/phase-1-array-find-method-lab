c94681f929656d99a74378bf1f513e164d8d3c7f
Chris Ding <chrisding7@berkeley.edu>
2022-08-30 11:18:02 -0700

    commit tree 9a08ee6c052127d5114a8a4ee47a71696b642994

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+function hasWon(element, key, array) {
+    return element.result === "W";
+}
+
+function superbowlWin(superbowlYears) {
+    const yearWonObject = superbowlYears.find(hasWon);
+    if(yearWonObject) {
+        return yearWonObject.year;
+    }
+    else {
+        return undefined;
+    }
+    //console.log(yearWon);
+    //return yearWon;
+}
+
+//superbowlWin(record);
